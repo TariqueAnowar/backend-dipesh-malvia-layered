eee0945b6e61d8666139547c1b3595198d45a712
Tarique Anowar <anowartarique5@gmail.com>
2025-01-01 23:12:31 +0530

    Added default fallback port to 4000

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const startServer = async () => {
 
     await App(app);
 
-    const PORT = process.env.PORT || 8001;
+    const PORT = process.env.PORT || 4000;
 
     server = app.listen(PORT, () => {
       console.log(`Server is running on port: ${PORT}`);
